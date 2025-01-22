2aee9df7e66c20b4edb601f4d0eefdc792332545
Sai <350696480@qq.com>
2025-01-22 19:16:21 +0800

    [Sai]LangSmith功能测试通过

--- Scripts/TestFiles/LangSmithTest.py
+++ Scripts/TestFiles/LangSmithTest.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 import openai
 from langsmith.wrappers import wrap_openai
 from langsmith import traceable
+from langsmith.client import Client
 
 input_content1 = """
 评价下下面两个的翻译结果
@@ -33,18 +34,47 @@ J、O、I、N、T分别代表一个不同的数字，满足下面的等式：
 其中，JOINT代表一个五位数，问JOINT是多少？
 """
 
+# 一个简单的数学问题
+problem2 = """
+一张方桌,砍掉一个角还剩下几个角
+"""
+
+problem3 = """
+桌子上有10只鸟，猎人开枪打死了了一只，还剩几只？
+"""
+
+problem4 = """
+有一个人是殡仪馆停尸间的看尸人，有一天他起床刷牙的时候发
+现自己的牙齿是绿色的，然后他就自杀了，为什么?
+提示:绿色为染料所染且颜色种类与题目无关系，看尸人有预感会发生这个事情
+"""
+
+problem5 = """
+马戏团里有两个侏儒，瞎子侏儒比另一个侏儒矮。马戏团只需要一个侏儒，
+马戏团的侏儒当然是越矮越好了。可是，在约定比个子的前一天，
+瞎子侏儒，也就是那个矮的侏儒已经在家里自杀死了，地上残留着许多碎木屑。他为什么自杀?
+提示:高矮侏儒之间并没有作任何身体接触，也没有语言接触
+"""
+
 # 加载.env文件中的环境变量
 load_dotenv()
 
 api_key = os.getenv('Deepseek_API_Key')
 api_url = os.getenv('Deepseek_API_URL')
-
 lanchain_api = os.getenv('LANGCHAIN_API_KEY')
 
+##临时设置环境变量
+###LANGCHAIN_TRACING_V2是设置LangChain是否开启日志跟踪模式。
+os.environ['LANGCHAIN_TRACING_V2'] = "true"
+###LANGCHAIN_API_KEY就是上面生成的LangSmith的key。
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+os.environ['LANGCHAIN_ENDPOINT'] = "https://api.smith.langchain.com"
+###LANGCHAIN_PROJECT 跟踪项目名称
+os.environ['LANGCHAIN_PROJECT'] = "test_"+datetime.datetime.now().strftime("%Y%m%d%H")
+
+
 client = OpenAI(api_key=api_key, base_url=api_url)
-
-client = wrap_openai(openai.Client())
-
+client = wrap_openai(client)
 
 @traceable # Auto-trace this function
 def pipeline(user_input: str):
@@ -52,13 +82,12 @@ def pipeline(user_input: str):
     start_time = datetime.datetime.now()
 
     response = client.chat.completions.create(
-        model= "deepseek-reasoner",
+        model="deepseek-reasoner",
         messages=[
-            # {"role": "system", "content": "你是一个经验丰富的数学家，请用中文思考和输出。"},
-            {"role": "user", "content": str},
+            {"role": "system", "content": "你是一个经验丰富的数学家，请用中文思考和输出。"},
+            {"role": "user", "content": user_input},
         ],
         stream=False
-        # stream=True
     )
 
     # 记录响应接收时间
@@ -102,5 +131,5 @@ def pipeline(user_input: str):
     else:
         print("No usage information available.")
 
-pipeline(problem1)
+pipeline(problem5)
 
